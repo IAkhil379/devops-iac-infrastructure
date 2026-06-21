a55b813180c0362716c8f2446440070642b65f22
I AKHIL <akhilelijah3@gmail.com>
2026-06-21 23:46:24 +0530

    feat: partition network into dedicated public and private subnets within availability zone a

---
+++ subnets.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-public-subnet-1"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags = {
+    Name        = "${var.environment}-private-subnet-1"
+    Environment = var.environment
+  }
+}
