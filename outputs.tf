fb669d8673f350d44f8b4aefed32542dedafe880
I AKHIL <151585189+IAkhil379@users.noreply.github.com>
2026-09-03 22:10:09 +0530

    Fix typo in resource reference for public IP output

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "vpc_id" {
 }
 
 output "web_server_public_ip" {
-  value       = aws_instance.web_server.public_ip
+  value       = aws_instant.web_server.public_ip
   description = "The dynamically assigned public interface route address to locate the web instance"
 }
