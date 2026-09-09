3675789f5bbfdb74e1b9f4bee63bf42ae0d3ff6c
I AKHIL <151585189+IAkhil379@users.noreply.github.com>
2026-09-09 23:34:08 +0530

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "${var.aws_region}n"
+  availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true
 
   tags = {
